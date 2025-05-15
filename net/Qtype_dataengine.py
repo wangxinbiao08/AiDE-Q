9ef4829be78f22870db9090d4448dcd52e04cba8
wangxinbiao08 <77318617+wangxinbiao08@users.noreply.github.com>
2025-05-15 11:51:14 +0800

    Update Qtype_dataengine.py

--- net/Qtype_dataengine.py
+++ net/Qtype_dataengine.py
@@ -16,7 +16,7 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
                      pretrain_model_path,  device="cuda:5",
                      **param):
 
-    logs = f'------------data-engine优化阶段开始----------------\n'
+    logs = f'------------data-engine start optimization----------------\n'
     with open(logs_save_path, 'a') as file:
         file.write(logs)
 
@@ -29,7 +29,7 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
     
 
 
-    collected_var_per_records = []   # 收集每个batch中records的confidence
+    collected_var_per_records = []   # collect confidence for the records in each batch
     collected_all_bits = []
     collected_all_recipes = []
     collected_all_renyi_Entropy = []
@@ -52,7 +52,7 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
         collected_all_param_J.append(param_J)
         collected_all_param_g.append(param_g)
         
-        # 收集n个子集的预测结果
+        # collect the prediction results of all subsets
         all_output = np.zeros((subset_num, current_batch_size, n_qubits_ls[0]))
         for s in range(subset_num):
             subset_size = int(dataengine_shots * subset_precentage)
@@ -71,7 +71,7 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
         collected_var_per_records.append(var_per_records)
     
     
-    # 合并每个batch的数据
+    # Concatenate the data of all batches
     collected_var_per_records = torch.cat(collected_var_per_records, dim=0)
     collected_all_bits = torch.cat(collected_all_bits, dim=0)
     collected_all_recipes = torch.cat(collected_all_recipes, dim=0)
@@ -80,11 +80,11 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
     collected_all_param_g = torch.cat(collected_all_param_g, dim=0)
 
 
-    # 提取方差较小的index
+    # Extract the index with small variance 
     sorted_indices = np.argsort(collected_var_per_records.numpy())
     min_variance_indices = sorted_indices[:max(1, int(collected_all_bits.size(0) * select_percentage))]
     
-    # 提取方差较小的数据
+    # Extract the data with small variance 
     collected_evaluation_bits = collected_all_bits[min_variance_indices, :, :]
     collected_evaluation_recipes = collected_all_recipes[min_variance_indices, :, :]
     c_input_data = (collected_evaluation_bits + collected_evaluation_recipes * 2).to(device).float()
@@ -100,16 +100,16 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
     c_output, _ = Net(c_input_data, c_combined_param)
     collected_evaluation_renyi_Entropy = c_output.cpu().detach()
         
-    # 获取剩余的索引
+    # Obtaining remaining indices
     remaining_indices = np.setdiff1d(np.arange(collected_all_bits.size(0)), min_variance_indices)
-    # 分割剩余的数据
+    # Obtaining the data related to the remaining indices
     collected_remaining_bits = collected_all_bits[remaining_indices, :, :]
     collected_remaining_recipes = collected_all_recipes[remaining_indices, :, :]
     collected_remaining_renyi_Entropy = collected_all_renyi_Entropy[remaining_indices, :]
     collected_remaining_param_J = collected_all_param_J[remaining_indices]
     collected_remaining_param_g = collected_all_param_g[remaining_indices]
 
-    # 保存所有数据
+    # Save all data
     if os.path.exists(evaluation_dataset_path):
         os.remove(evaluation_dataset_path)
     with h5py.File(evaluation_dataset_path, 'a') as f:
@@ -126,7 +126,7 @@ def Qtype_dataengine(evaluation_dataset_path, remaining_dataset_path, logs_save_
 
     if os.path.exists(remaining_dataset_path):
         os.remove(remaining_dataset_path)
-    with h5py.File(remaining_dataset_path, 'w') as f:  # 使用 'w' 模式写入新数据
+    with h5py.File(remaining_dataset_path, 'w') as f:  
         for record_index, (bits, recipes, renyi_Entropy_3q, param_J, param_g) in enumerate(
                 zip(collected_remaining_bits, collected_remaining_recipes, collected_remaining_renyi_Entropy, collected_remaining_param_J, collected_remaining_param_g)):
             record_group = f.create_group(f'record_{record_index}')
